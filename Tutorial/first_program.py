c637f7aa68f00127943a2483905cab5f98e6a4cb
Sonu637815 <shersiya.sonu31@gmail.com>
2025-09-30 11:37:24 +0530

    new

---
+++ Tutorial/first_program.py
@@ -0,0 +1,3 @@
+print("My First Program")
+print("Hello, World!")
+print("Let's start counting ... 1 2 3 4 5 6 ...... ")
---
+++ Tutorial/variables.py
@@ -0,0 +1,10 @@
+name = "Sonu"          ## String variable
+age = 21               ## Integer variable
+
+name = "Monu"
+age = 20
+
+is_adult = True      ## Boolean variable
+print(name)
+print(age)
+print(is_adult)
